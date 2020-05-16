3200434712f7ed03af931df3eb21a2afd3f2421f
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2020-05-16 23:19:04 +0200

    Uni of set of fruit and distroless dockerfile

--- src/main/java/com/serrodcal/poc/service/FruitService.java
+++ src/main/java/com/serrodcal/poc/service/FruitService.java
@@ -1,13 +1,11 @@
 package com.serrodcal.poc.service;
 
 import com.serrodcal.poc.domain.Fruit;
-//import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-//import java.util.List;
 import java.util.Set;
 
 @ApplicationScoped
@@ -24,7 +22,6 @@ public class FruitService {
 
     public Uni<Set<Fruit>> getFruits() {
         return Uni.createFrom().item(this.fruits);
-        //return Multi.createFrom().items(this.fruits.stream());
     }
 
 }
