50c4e60c1a80a78d40a9be5aa0c4c65611e395ac
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2020-05-18 08:53:23 +0200

    added set to avoid reflection warning

--- src/main/java/com/serrodcal/poc/resource/FruitResource.java
+++ src/main/java/com/serrodcal/poc/resource/FruitResource.java
@@ -3,10 +3,13 @@ package com.serrodcal.poc.resource;
 import javax.enterprise.context.ApplicationScoped;
 
 import com.serrodcal.poc.service.FruitService;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.RoutingContext;
 import org.jboss.logging.Logger;
 
 import io.quarkus.vertx.web.Route;
-import io.quarkus.vertx.web.RoutingExchange;
 import io.vertx.core.http.HttpMethod;
 
 @ApplicationScoped
@@ -14,18 +17,20 @@ public class FruitResource {
 
     private static final Logger logger = Logger.getLogger(FruitResource.class);
 
+    private final String APPLICATION_JSON = "application/json";
+
     private FruitService fruitService;
 
     FruitResource(FruitService fruitService) {
         this.fruitService = fruitService;
     }
 
-    @Route(path = "/api/v1/fruit",methods = HttpMethod.GET)
-    void getFruits(RoutingExchange ex) {
+    @Route(path = "/api/v1/fruit", methods = HttpMethod.GET)
+    void getFruits(RoutingContext rc) {
         logger.info("FruitResource.getFruits()");
-        this.fruitService.getFruits().subscribe().with(
-                result -> ex.ok(result.toString()),
-                failure -> ex.serverError()
+        Uni.createFrom().item(this.fruitService.getFruits()).subscribe().with(
+                result -> rc.response().putHeader(HttpHeaders.CONTENT_TYPE, this.APPLICATION_JSON).end(Json.encode(result)),
+                failure -> rc.fail(500)
         );
     }
 }
